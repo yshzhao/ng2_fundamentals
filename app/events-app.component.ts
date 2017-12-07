5cf8b769538407be81ad59952877418be8a48d5e
Michael Burkhart <mburkhart41@gmail.com>
2017-12-07 18:55:05 -0500

    Added Services

---
+++ app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'events-app',
+    template: `<nav-bar></nav-bar><events-list></events-list>`
+})
+export class EventsAppComponent {
+
+}
